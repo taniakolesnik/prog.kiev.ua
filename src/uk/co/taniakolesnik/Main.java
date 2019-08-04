66ed2a329003860ac86810bb1a562f140d0d29f6
Tania Kolesnik <kolesnik@gmail.com>
2019-08-05 00:15:52 +0100

    fixed List and overwrote equals and hashcode

--- src/uk/co/taniakolesnik/Main.java
+++ src/uk/co/taniakolesnik/Main.java
@@ -3,23 +3,21 @@ package uk.co.taniakolesnik;
 public class Main {
 
     public static void main(String[] args) {
-        Network network = new Network("Three");
-        network.printPhoneNumbersRegistered();
+        Network network = new Network("Three");git
 
         Phone phoneTania = new Phone();
         phoneTania.setNumber(11111);
         phoneTania.setOwner("Tania");
         phoneTania.call(22222); // call FROM unregistered number
+
         phoneTania.registerPhone(network); //register number one
 
         Phone phoneKaty = new Phone(22222,"Katy");
-
         phoneTania.call(22222); // call TO unregistered number
 
         phoneKaty.registerPhone(network); //register number two
         phoneTania.call(22222);
 
         network.printPhoneNumbersRegistered();
-
     }
 }
