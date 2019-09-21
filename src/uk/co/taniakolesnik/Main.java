d255034e686a317bb7ba0f43af435c539ebc068d
Tania Kolesnik <kolesnik@gmail.com>
2019-09-21 13:08:18 +0100

    done

--- src/uk/co/taniakolesnik/Main.java
+++ src/uk/co/taniakolesnik/Main.java
@@ -1,93 +1,25 @@
 package uk.co.taniakolesnik;
 
-import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    private static Map<String, String> dictionary;
 
     public static void main(String[] args) {
-        dictionary  = new HashMap<>();
-        dictionary.put("hello", "привіт");
 
-        translate("hello world");
-        askForWorldTranslation();
-        translate("hello world");
-        translate("world hello");
+        String[] strings = new String[]{"one", "two", "two", "six", "six", "six", "six", "six", "six"};
+        Integer[] integers = new Integer[]{1,2,3,4,5,6,2,2,2,2,2,5,5,5,1,1,1};
 
-        System.out.println(dictionary);
-        saveDictionary();
-
-        // remove one line from current dictionary and load saved one
-        dictionary.remove("world");
-
-        System.out.println("current dictionary in use: " + dictionary);
-        loadDictionary();
-        System.out.println("loaded old dictionary: " + dictionary);
-
+        count(strings);
+        count(integers);
     }
 
-    private static void askForWorldTranslation() {
-
-        System.out.println("Enter new word in english (e.g. world): ");
-        Scanner scanner_key = new Scanner(System.in);
-        String key = scanner_key.next();
-        System.out.println("Enter translation for " + key + " (e.g. світ):");
-        Scanner scanner_value = new Scanner(System.in);
-        String value = scanner_value.next();
-        dictionary.put(key, value);
+    private static <T> void count( T ... args){
+        List<T> list = Arrays.asList(args);
+        Map<T, Long> map = list.stream()
+                .collect(Collectors.groupingBy(s->s, Collectors.counting()));
+        System.out.println(map);
     }
-
-    private static String translate(String sentence) {
-        List<String> words = Arrays.asList(sentence.split("\\W"));
-        String translation = words
-                .stream()
-                .map(Main::getTranslatedWord)
-                .collect(Collectors.joining(" "));
-
-        System.out.println(translation);
-        saveTranslation(translation,"Ukrainian.out");
-        return translation;
-    }
-
-    public static String getTranslatedWord(String word){
-        String translation = word;
-        if (dictionary.get(word)!=null){
-            translation = dictionary.get(word);
-        }
-        return translation;
-    }
-
-    public static void saveTranslation(String translation, String fileName){
-
-        File file = new File("C:\\Users\\TanyaK\\Downloads\\" + fileName);
-        try ( PrintWriter writer = new PrintWriter(file)){
-            writer.println(translation);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void saveDictionary(){
-
-        File file = new File("C:\\Users\\TanyaK\\Downloads\\dictionary");
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))){
-            objectOutputStream.writeObject(dictionary);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void loadDictionary(){
-
-        File file = new File("C:\\Users\\TanyaK\\Downloads\\dictionary");
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))){
-            dictionary = (Map<String, String>) objectInputStream.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
+
